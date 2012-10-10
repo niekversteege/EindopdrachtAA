1bd3a96c4f09dcd562fb5926c617f5ec47efbf4a
Niek <niekversteege@gmail.com>
2012-10-10 14:52:59 +0200

    Init

---
+++ EindopdrachtAA/src/nl/niek/minor/aa/eindopdracht/Main.java
@@ -0,0 +1,13 @@
+package nl.niek.minor.aa.eindopdracht;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
