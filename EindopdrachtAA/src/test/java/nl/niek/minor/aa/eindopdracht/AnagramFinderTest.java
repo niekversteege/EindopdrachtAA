25d2c872d4c2cdeeeda81f1fcb20213156a671f9
Niek <niekversteege@gmail.com>
2012-10-31 00:03:31 +0100

    Maven layout + test.

---
+++ EindopdrachtAA/src/test/java/nl/niek/minor/aa/eindopdracht/AnagramFinderTest.java
@@ -0,0 +1,22 @@
+package nl.niek.minor.aa.eindopdracht;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AnagramFinderTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
